c1fb31c508eed5d954c192c4e3416925d2890657
sobon_lukasz_241236 <sobonukasz@gmail.com>
2024-12-08 11:10:34 +0100

    Fix displaying drawer in correct window.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingPanelAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingPanelAdapter.java
@@ -4,13 +4,14 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawingPanelAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawingPanelAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
-
+	private DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
 	public DrawingPanelAdapter() {
 		super();
 	}
@@ -27,7 +28,7 @@ public class DrawingPanelAdapter extends DrawPanelController implements Job2dDri
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawPanelController.drawLine(line);
 		this.setPosition(x, y);
 	}
 
