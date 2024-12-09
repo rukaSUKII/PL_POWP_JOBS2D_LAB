1d3d8e6f5c0e52392d6e743300a332ba2e89d2b9
rukaSUKII <sobonukasz@gmail.com>
2024-12-09 20:48:34 +0100

    Add adapter for special line. Add option in context menu to use new line

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/SpecialLineDrawingPanelAdapter.java
@@ -0,0 +1,39 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+/**
+ * driver adapter to drawer with several bugs.
+ */
+public class SpecialLineDrawingPanelAdapter implements Job2dDriver {
+    private int startX = 0, startY = 0;
+    private final DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
+    public SpecialLineDrawingPanelAdapter() {
+        super();
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getSpecialLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+
+        drawPanelController.drawLine(line);
+        this.setPosition(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Special Line Drawing Panel";
+    }
+}
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawingPanelAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.SpecialLineDrawingPanelAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -42,6 +43,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new DrawingPanelAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver differentLineDriver = new SpecialLineDrawingPanelAdapter();
+		DriverFeature.addDriver("Different Line Driver", differentLineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
