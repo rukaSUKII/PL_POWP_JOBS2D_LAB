b2325728d3ad5f7e810741005cbf713f96f76887
Lukasz Sobon <241236@edu.p.lodz.pl>
2024-12-15 10:30:46 +0100

    Add class hierarchy from UML diagram

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.command;
+
+public class OperateToCommand implements DriverCommand{
+    private int x;
+    private int y;
+
+    @Override
+    public void execute(){
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.command;
+
+public class SetPositionCommand implements DriverCommand{
+    private int x;
+    private int y;
+
+    @Override
+    public void execute(){
+
+    }
+}
