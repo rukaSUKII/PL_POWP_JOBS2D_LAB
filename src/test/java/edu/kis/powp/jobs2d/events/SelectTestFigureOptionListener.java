dc11f06b1a18cac56ec34127790389d7d8b1ea2e
Lukasz Sobon <241236@edu.p.lodz.pl>
2024-12-15 11:25:32 +0100

    Implement usage of command in application

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,24 +3,30 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.command.*;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+    private DriverManager driverManager;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
-		this.driverManager = driverManager;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equalsIgnoreCase("Figure Joe 1")) {
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		}
-		else{
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equalsIgnoreCase("Figure Joe 1")) {
+            FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        } else if (e.getActionCommand().equalsIgnoreCase("Figure Joe 2")) {
+            FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+        } else if (e.getActionCommand().equalsIgnoreCase("Rectangle")){
+            DriverCommand commands = CommandFactory.Rectangle(driverManager.getCurrentDriver(), 0, 0, 50, 50);
+            commands.execute();
+        }else if (e.getActionCommand().equalsIgnoreCase("Test shape")){
+            DriverCommand commands = CommandFactory.TestShape(driverManager.getCurrentDriver(), 0, 0, 50, 50);
+            commands.execute();
+        }
+    }
 }
